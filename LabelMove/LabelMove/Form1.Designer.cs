8e98d00d4315758a662773ca913f6726237d1034
Lettuce <85661679+CLettuce@users.noreply.github.com>
2022-12-20 16:30:49 -0600

    Add files via upload

---
+++ LabelMove/LabelMove/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace LabelMove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.moveL1 = new LabelMove.MoveL();
+            this.SuspendLayout();
+            // 
+            // moveL1
+            // 
+            this.moveL1.Font = new System.Drawing.Font("DS-Digital", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveL1.Location = new System.Drawing.Point(12, 165);
+            this.moveL1.Name = "moveL1";
+            this.moveL1.Size = new System.Drawing.Size(790, 113);
+            this.moveL1.TabIndex = 0;
+            this.moveL1.Text = "Movimiento de letra";
+            this.moveL1.UseCompatibleTextRendering = true;
+            this.moveL1.Velocidad_Text = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.moveL1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MoveL moveL1;
+    }
+}
+
---
+++ LabelMove/LabelMove/MoveL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelMove
+{
+    public partial class MoveL : Label
+    {
+        public MoveL()
+        {
+            InitializeComponent();
+            UseCompatibleTextRendering = true;
+            //this.BackColor = Color.White;
+            timer1.Start();
+        }
+
+        int position, speed;
+
+        public int Velocidad_Text
+        {
+            get { return speed; }
+            set { speed = value; Invalidate(); }
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.TranslateTransform((float)position, 0);
+            base.OnPaint(e);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Size size = TextRenderer.MeasureText(this.Text, this.Font);
+            if (size.Width <= base.Width)
+            {
+
+                base.Invalidate();
+            }
+
+            if (position < -Width)
+            {
+                position = Width;
+
+            }
+            position -= speed;
+            Invalidate();
+        }
+
+    }
+}
